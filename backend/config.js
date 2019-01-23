b00600d2679efb3263803e290268822592174f81
Maddie Nelson <mrn14@duke.edu>
2019-01-22 20:41:49 -0500

    trying to pull from the changes they made.

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,8 @@
 // config.js
 const config = {
-    dbUri: "mongodb://marzelinger:metameals123@ds058739.mlab.com:58739/meta-meals"
+    //dbUri: "mongodb://marzelinger:metameals123@ds058739.mlab.com:58739/meta-meals"
+    //the above db is used to test the database for the login stuff.
+    dbUri: "mongodb://madeline:madeline32897@ds111025.mlab.com:11025/login_auth_app"
   };
   
   export const getConfig = key => config[key];
