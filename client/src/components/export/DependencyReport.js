087c6de171772cd9046b1048511e9752c9ceaaec
Maddie Nelson <mrn14@duke.edu>
2019-02-05 01:51:43 -0500

    this is stable

--- client/src/components/export/DependencyReport.js
+++ client/src/components/export/DependencyReport.js
@@ -41,6 +41,8 @@ async loadDataFromServerForReport(ingredients){
     for(let ing = 0; ing<count ; ing++){
         var curData = ingredients[ing];
         var dataLine = [];
+        finalData.push("INGREDIENTS");
+        finalData.push("\r\n");
         dataLine.push(curData.num);
         dataLine.push(curData.name);
         dataLine.push(curData.vendor_info);
@@ -49,7 +51,9 @@ async loadDataFromServerForReport(ingredients){
         dataLine.push(curData.comment);
         //dataLine.push("\r\n");
         finalData.push(dataLine);
-        finalData.push("\r\n");
+        //finalData.push("\r\n");
+        finalData.push("SKUS");
+        //finalData.push("\r\n");
         //var ingSKUS = curData.skus.length;
         console.log("this is the dataline: "+dataLine);
         //console.log("this is the ingSkus: "+ingSKUS);
@@ -84,10 +88,10 @@ async loadDataFromServerForReport(ingredients){
 
 
           curSku.push(curSkuObj.comment);
-          //curSku.push("\r\n");
+          curSku.push("\r\n");
           console.log("this is the curSku: "+curSku);
           finalData.push(curSku);
-          finalData.push("\r\n");
+          //finalData.push("\r\n");
           console.log("this is the skuData: "+finalData);
         }
       }
@@ -101,45 +105,6 @@ async loadDataFromServerForReport(ingredients){
     }    
     fileDownload(finalData, fileTitle+'.csv');
 };
-
-
-getSKUSbyIngId = (ingID) => {
-    console.log('this is the ingID: '+ ingID);
-    var skuData = [];
-    // var res = await SubmitRequest.submitGetFilterData(Constants.sku_filter_path, 
-    //       "_", ingID, "_", "_");
-    //       console.log("this is the res: "+res);
-
-    //   // if (!res.success) {
-      // this.setState({ error: res.error });
-      // }
-      // else {
-      //   var resData = res.data;
-      //   console.log("this is the skuData: "+resData);
-      //   console.log("this is the skuData string: "+JSON.stringify(resData));
-      //   for(let s = 0; s<resData.length; s++){
-      //     var curSku = [];
-      //     var curSkuObj = resData[s];
-      //     curSku.push(curSkuObj.num);
-      //     console.log("this is the num: "+curSkuObj.num);
-      //     curSku.push(curSkuObj.name);
-      //     curSku.push(curSkuObj.case_cpc);
-      //     curSku.push(curSkuObj.unit_upc);
-      //     curSku.push(curSkuObj.unit_size);
-      //     curSku.push(curSkuObj.cpc);
-      //     curSku.push(curSkuObj.prod_line);
-      //     curSku.push(curSkuObj.comment);
-      //     curSku.push("\r\n");
-      //     console.log("this is the curSku: "+curSku);
-      //     skuData.push(curSku);
-      //     console.log("this is the skuData: "+skuData);
-      //   }
-      //   console.log("this is the skuLine: "+ curSku);
-      //}
-    
-    return skuData;
-
-}
 
 render() {
 return (
