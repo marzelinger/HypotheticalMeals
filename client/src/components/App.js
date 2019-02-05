b7c4077c61f7b5449e90cd46c71f51dca834ffbc
Maddie Nelson <mrn14@duke.edu>
2019-02-04 21:53:01 -0500

    this is stable and is fixing the logout issue.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -45,7 +45,7 @@ class App extends React.Component{
 
   determineUser = () => {
     if (localStorage.jwtToken) {
-      if(localStorage.getItem("firstAdminCreated")){
+      //if(localStorage.getItem("firstAdminCreated")){
         // Set auth token header auth
         const token = localStorage.jwtToken;
         setAuthToken(token);
@@ -62,7 +62,7 @@ class App extends React.Component{
           // Redirect to login
           window.location.href = "./login";
         }
-      }
+      //}
     }
   }
 
