0c097ccb03e9b1e40b1c797fb87f6002a8dd890a
Maddie Nelson <mrn14@duke.edu>
2019-02-06 01:53:08 -0500

    the logout isn't working.

--- client/src/components/UserMenu.js
+++ client/src/components/UserMenu.js
@@ -58,7 +58,8 @@ export default class UserMenu extends React.Component {
             >
                 <Menu>
                     <MenuItem primaryText = "LogoutDUMMY"></MenuItem>
-                    <Logout></Logout>
+                    {//<Logout></Logout>
+                    }
                     
                 </Menu>
             </Popover>
