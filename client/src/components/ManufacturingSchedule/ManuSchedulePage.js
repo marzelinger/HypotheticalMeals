112a48e249ef1ace08b5c3d0a51ca76831fee3c8
Riley Nisbet <rn67@duke.edu>
2019-02-20 13:19:14 -0500

    tried react-calendar-timeline, reverting back tho

--- client/src/components/ManufacturingSchedule/ManuSchedulePage.js
+++ client/src/components/ManufacturingSchedule/ManuSchedulePage.js
@@ -1,105 +1,37 @@
-// ManuSchedulePage.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+import moment from 'moment'
+import Timeline from 'react-calendar-timeline'
 
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import Timeline from 'react-visjs-timeline'
-
-export default class ManuSchedulePage extends Component {
+export default class ManuSchedulePage extends React.component {
     constructor(props) {
         super(props)
-        
-        this.state = {
-            groups : [],
-            items: [],
-            options: {}
-        }
-        this.initiateData();
     }
-
-    initiateData() {
-        this.state.items = [
-            {
-                start: new Date(2019, 2, 19, 8, 30),
-                end: new Date(2019, 2, 19, 10, 45), 
-                content: 'Biology',
-                title: '202L',
-                group: 1,
-                editable: true
-            },
-            {
-                start: new Date(2019, 2, 19, 10, 30),
-                end: new Date(2019, 2, 19, 11, 45),  
-                content: 'Maths',
-                title: '216D',
-                group: 2,
-                editable: true
-            },
-            {
-                start: new Date(2019, 2, 19, 13, 30),
-                end: new Date(2019, 2, 19, 15, 45), 
-                content: 'EGR',
-                title: '201L',
-                group: 2,
-                editable: true
-            }
-        ]
-        this.state.groups = [
-            {
-                id: 1,
-                content: 'Group A',
-            },
-            {
-                id: 2,
-                content: 'Group B',
-            },
-            {
-                id: 3,
-                content: 'Group C',
-            },
-            {
-                id: 4,
-                content: 'Group D',
-            }
-        ]
-        this.state.options = {
-            width: '100%',
-            maxHeight: 50 + 4*35 + 'px',
-            stack: false,
-            showMajorLabels: true,
-            showCurrentTime: true,
-            zoomMin: 1000000,
-            type: 'box',
-            format: {
-                minorLabels: {
-                    minute: 'h:mma',
-                    hour: 'ha'
-                }
-            },
-            selectable: true,
-            onMove: function (item, callback) {
-                console.log('yo')
-                if (item.group == 2) {
-                    callback(null)
-                } else {
-                    callback(item)
-                }
-            }
-        }
-    }
-
-    onRemove() {
-        alert('no!')
-    }
-
+    
     render() {
-        return (
-        <div>
-            <Timeline 
-                options={this.options}
-                items={this.state.items}
-                groups={this.state.groups}
-            />
-        </div>
-        );
+        return (<div id='root'></div>)
     }
 }
+
+const groups = [
+  {id: 1, title: 'group 1'},
+  {id: 2, title: 'group 2'}
+]
+
+const items = [
+  {id: 1, group: 1, title: 'item 1', start_time: moment(), end_time: moment().add(1, 'hour')},
+  {id: 2, group: 2, title: 'item 2', start_time: moment().add(-0.5, 'hour'), end_time: moment().add(0.5, 'hour')},
+  {id: 3, group: 1, title: 'item 3', start_time: moment().add(2, 'hour'), end_time: moment().add(3, 'hour')}
+]
+
+ReactDOM.render(
+  <div>
+    Rendered by react!
+    <Timeline groups={groups}
+              items={items}
+              defaultTimeStart={moment().add(-12, 'hour')}
+              defaultTimeEnd={moment().add(12, 'hour')}
+              />
+  </div>,
+  document.getElementById('root')
+);
