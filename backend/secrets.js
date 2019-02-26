f841c8acc7c3e4c070898a2bde33a97fb5870384
Maddie Nelson <mrn14@duke.edu>
2019-02-26 16:44:15 -0500

    this is formula detail stuff

--- backend/secrets.js
+++ backend/secrets.js
@@ -2,6 +2,7 @@
 const secrets = {    
     // dbUri: "mongodb://admin:test123@ds227255.mlab.com:27255/meta-meals-testing",
     dbUri: "mongodb://admin:testtest123@ds056288.mlab.com:56288/meta-production",
+
     secretOrKey: "secret",
     client_secret: "zHMB4Sl*o*Awu*mjZv$VEa+fX=QACLIWuRNWyNe@kNtTYLd*4E"
   };
--- client/src/components/ListPage/SkuFormulaDetails.js
+++ client/src/components/ListPage/SkuFormulaDetails.js
@@ -22,6 +22,7 @@ import DataStore from '../../helpers/DataStore'
 
 
 
+
 import Filter from './Filter'
 
 const currentUserIsAdmin = require("../auth/currentUserIsAdmin");
@@ -191,20 +192,20 @@ export default class SkuFormulaDetails extends React.Component {
 
     }
 
-    formulaButtonsCreate = async() => {
-        let res = await submitRequest.getData(Constants.formulas_page_name);
-        var formulasExist = false;
-        if(res.success){
-            if(res.data>0){
-                formulasExist = true;
-            }
-        }
+    formulaButtonsCreate = () => {
+        // let res = await SubmitRequest.getData(Constants.formulas_page_name);
+        // var formulasExist = false;
+        // if(res.success){
+        //     if(res.data>0){
+        //         formulasExist = true;
+        //     }
+        // }
         return (
             <div>
                 <Button color="primary" onClick = {() => this.handleNewFormula()} >Create New Formula</Button>{' '}
-                {formulasExist? 
-                (<Button color="secondary" onClick = {() => this.handleExistingFormula()} >Add Existing Formula</Button>)
-                 : <div/>}
+                {/* {formulasExist?  */}
+                <Button color="secondary" onClick = {() => this.handleExistingFormula()} >Add Existing Formula</Button>
+                 {/* : <div/>} */}
             </div>
         );
     }
