9cbab654e7e3a1267c66d9f6bd6a7c62d029b7d0
Maddie Nelson <mrn14@duke.edu>
2019-02-26 11:46:36 -0500

    connected to new db

--- backend/secrets.js
+++ backend/secrets.js
@@ -1,6 +1,7 @@
 // secrets.js
 const secrets = {    
-    dbUri: "mongodb://admin:test123@ds227255.mlab.com:27255/meta-meals-testing",
+    // dbUri: "mongodb://admin:test123@ds227255.mlab.com:27255/meta-meals-testing",
+    dbUri: "mongodb://admin:testtest123@ds056288.mlab.com:56288/meta-production",
     secretOrKey: "secret",
     client_secret: "zHMB4Sl*o*Awu*mjZv$VEa+fX=QACLIWuRNWyNe@kNtTYLd*4E"
   };
