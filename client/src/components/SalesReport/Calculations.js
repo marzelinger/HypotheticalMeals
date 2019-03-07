c1fc5650e49244c0e4f7a83dc6a01f37a6057792
Maddie Nelson <mrn14@duke.edu>
2019-03-07 11:28:20 -0500

    this is calculating the 10 yr data and now about to calculate the totals.

--- client/src/components/SalesReport/Calculations.js
+++ client/src/components/SalesReport/Calculations.js
@@ -55,6 +55,9 @@ export default class Calculations{
             for (let s = 0; s<skus.length; s++){
                 // //need to go through all the prodlines. and then through all the skus.
                 var curSkuData = [];
+                var cur_ten_yr_sum = 0;
+                var cur_ten_yr_sales = 0;
+                var cur_ten_yr_avg_case = 0;
                 for(let yr = 0; yr<dateRanges.length; yr++){
                     let datares = await SubmitRequest.submitGetSaleRecordsByFilter('_', cust_str, '_', skus[s]._id, 
                     dateRanges[yr]['startdate'], dateRanges[yr]['enddate'], 0, 0);
@@ -62,15 +65,20 @@ export default class Calculations{
                         console.log("this is the dateres: "+JSON.stringify(datares));
                         var curRowData = await Calculations.getSalesTotalPerYear(datares.data);
                         console.log("this is the curRowData: "+JSON.stringify(curRowData));
+                        cur_ten_yr_sum += curRowData.yr_revenue;
+                        cur_ten_yr_sales += curRowData.yr_sales;
+                        cur_ten_yr_avg_case += curRowData.yr_avg_rev_per_case;
                         curSkuData.push({ yr: yr, salesData: curRowData });
                     }
 
                 }
-                tenYRSKUsdata.skus.push({sku: skus[s]._id, skuData: curSkuData});
+                cur_ten_yr_avg_case = cur_ten_yr_avg_case/10;
+                var {skuTotalData} = this.calcTotalData(skus[s], cur_ten_yr_sum, cur_ten_yr_sales, cur_ten_yr_avg_case);
+                tenYRSKUsdata.skus.push({sku: skus[s]._id, skuData: curSkuData, totalData: skuTotalData});
+
                 console.log("cur ten yr: "+ JSON.stringify(tenYRSKUsdata));
             }
         }
-
         return tenYRSKUsdata;
     }
 
@@ -88,9 +96,24 @@ export default class Calculations{
             avg_rev_per_case = total_rev/total_cases;
         }
         console.log("total rev: "+total_rev + " avg per case: "+avg_rev_per_case);
-        return { yr_revenue: total_rev, yr_avg_rev_per_case: avg_rev_per_case};
+        return { yr_revenue: total_rev, yr_avg_rev_per_case: avg_rev_per_case, yr_sales: total_cases};
     }
 
+
+
+    static async calcTotalData(sku, tenYRrev, tenYRsales, tenYRcaseavg){
+        var sum_yearly_rev = 0;
+        var avg_manu_run_size = 0; //find the run size for all the activities for this sku and then divide by the number of activities
+        var ingr_cost_per_case = 0;
+        var avg_manu_setup_cost_per_case = 0;
+        var manu_run_cost_per_case = 0;
+        var total_COGS_per_case = 0;
+        var avg_rev_per_case = 0; // = revenue sum / numsales
+        var avg_profit_per_case = 0; // = avg_rev - cogs
+        var profit_marg = 0;
+
+
+    }
 
 
 
