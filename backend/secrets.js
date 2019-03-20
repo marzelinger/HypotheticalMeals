f5b7a42419b6d8bfdc121dd802103e702e870362
Belal Taher <belal.taher@duke.edu>
2019-03-20 14:12:30 -0400

    Fixed some formula stuff

--- backend/secrets.js
+++ backend/secrets.js
@@ -1,6 +1,6 @@
 // secrets.js
 const secrets = {    
-    dbUri: "mongodb://admin:testtest123@ds211083.mlab.com:11083/meta-meals-ev3-testing",
+    dbUri: "mongodb://admin:testtest123@ds129651.mlab.com:29651/meta-meals-backup-testing-ev3",
     secretOrKey: "secret",
     client_secret: "zHMB4Sl*o*Awu*mjZv$VEa+fX=QACLIWuRNWyNe@kNtTYLd*4E"
   };
@@ -11,3 +11,6 @@ const secrets = {
 //testing DB: mongodb://admin:test123@ds227255.mlab.com:27255/meta-meals-testing
 //prod DB: mongodb://admin:testtest123@ds056288.mlab.com:56288/meta-production
 //mongodb://<dbuser>:<dbpassword>@ds211083.mlab.com:11083/meta-meals-ev3-testing
+
+//backup/dump testing
+//mongodb://admin:testtest123@ds129651.mlab.com:29651/meta-meals-backup-testing-ev3
